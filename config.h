e566dbdc7f0eeefe1f5690a8775529f0b5f398c8
yamage <chocomate.dowob.y912@gmail.com>
2024-08-14 00:23:16 +0900

    enable split, pwm3360

--- config.h
+++ config.h
@@ -18,3 +18,19 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+#define MASTER_RIGHT
+
+#define SPLIT_HAND_PIN GP10
+
+// https://docs.qmk.fm/drivers/serial#setup-2
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN GP12     // USART TX pin
+#define SERIAL_USART_RX_PIN GP13     // USART RX pin
+
+/* SPI & PMW3360 settings. */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP2
+#define SPI_MISO_PIN GP3
+#define SPI_MOSI_PIN GP4
+#define POINTING_DEVICE_CS_PIN GP5
--- rules.mk
+++ rules.mk
@@ -1,1 +1,7 @@
 # This file intentionally left blank
+
+# https://docs.qmk.fm/drivers/serial#setup-2
+SERIAL_DRIVER = vendor
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pmw3360
